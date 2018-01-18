3b2205461ae76317915e817280f5387ed44e21bc
wansuisu <yksdtt@gmail.com>
2018-01-18 10:26:15 -0600

    Add files via upload

---
+++ AdalineSGD.py
@@ -0,0 +1,70 @@
+from numpy.random import seed
+import numpy as np
+
+class AdalineSGD(object):
+
+    def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+
+    def fit(self, X, y):
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost)/len(y)
+            self.cost_.append(avg_cost)
+        return self
+
+    def partial_fit(self, X, y):
+        """Fit training data without reinitializing the weights"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _initialize_weights(self, m):
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True;
+
+    def _shuffle(self, X, y):
+        """Shuffle training data"""
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+
+    def _update_weights(self, xi, target):
+        """Apply Adaline learning rule to update the weights"""
+        output = self.net_input(xi)
+        error = target - output
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error**2
+        return cost
+
+    def activation(self, X):
+        """Compute linear activation"""
+        return self.net_input(X)
+
+    def predict(self, X):
+        """return class label after unit step"""
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+
+
+
